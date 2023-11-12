e080f8d2b1c93e2d04ef61b4fcd167f61af8e52d
Dmitri Konovalenko <dima92konovalenko@gmail.com>
2023-11-12 20:12:08 +0100

    update

--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
 
-use Framework\App;
+use Plutuss\SauceCore\App;
 
 define('APP_DIR', __DIR__);
+
 header("Content-Type: text/html");
 
 require_once APP_DIR . '/../vendor/autoload.php';
+
 session()->sessionCheckTimeout();
+
 require_once APP_DIR . '/../routes/web.php';
 
 $app = new App();
+
 $app->run();
