1680949d98bdc0816ab6e54e6d5aed396c567ccc
Dmitri Konovalenko <dima92konovalenko@gmail.com>
2023-11-13 19:32:59 +0100

    update:

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\HomeController;
 use Plutuss\SauceCore\Routing\Route;
 
-/**
- *  
- * 
- */
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application.
+|
+*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
