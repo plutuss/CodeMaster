ebbf4776efe145fb87aec5658d48d23af31ee72a
Dmitri Konovalenko <dima92konovalenko@gmail.com>
2023-11-14 11:19:19 +0100

    add: Console

---
+++ app/Console/Commands/HelloworldCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Console\Commands;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class HelloworldCommand  extends Command
+{
+    protected function configure()
+    {
+        $this->setName('hello-world')
+            ->setDescription('Prints Hello-World!')
+            ->setHelp('Demonstration of custom commands created by Symfony Console component.')
+            ->addArgument('username', InputArgument::REQUIRED, 'Pass the username.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf('Hello World!, %s', $input->getArgument('username')));
+        return Command::SUCCESS;
+    }
+}
---
+++ config/commands.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Console\Commands\HelloworldCommand;
+
+return [
+
+    'register' => [
+        HelloworldCommand::class,
+    ]
+];
